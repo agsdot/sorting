c5acc844b97a1a6ee5df40768f0643955b5f1cf4
Jonathan Lai <jonathanlai@eltux.net>
2013-06-06 23:39:34 -0700

    Added Rspec tests

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,5 +1,9 @@
 require 'pry'
 
+require 'rspec'
+require 'benchmark'
+
+
 def sortarray(x)
   # comparisons = 0
   n = x.size # 7
@@ -27,8 +31,46 @@ def sortarray(x)
 end # def
 
 
-array = (50..60).to_a.shuffle!
+array = (1..10000).to_a.shuffle!
 # puts '--------------'
+
 puts sortarray(array)
 
+
+
+describe "#sortarray" do
+  it "gives the correct first number" do
+    array = [49,23,133,9]
+    result = sortarray(array)
+    expect(result[0]).to eq 9
+  end
+
+  it "gives the correct 2nd number" do
+    array = [49,23,133,9]
+    result = sortarray(array)
+    expect(result[1]).to eq 23
+  end
+
+  it "gives the correct 3rd number" do
+    array = [49,23,133,9]
+    result = sortarray(array)
+    expect(result[2]).to eq 49
+  end
+
+  it "gives the correct last number" do
+    array = [49,23,133,9]
+    result = sortarray(array)
+    expect(result[3]).to eq 133
+  end
+
+
+end
+
+
+    # it "pops off the last node of the list" do
+    #   list = List.new
+    #   list.push(5)
+    #   list.pop
+    #   expect(list.size).to eq 0
+    # end
 
