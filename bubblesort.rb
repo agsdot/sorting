269d2e12cb709b87e5cf896c3bb0339777056270
Jonathan Lai <jonathanlai@eltux.net>
2013-06-06 23:17:23 -0700

    commented out inspection code

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,18 +1,18 @@
 require 'pry'
 
 def sortarray(x)
-  comparisons = 0
+  # comparisons = 0
   n = x.size # 7
   x.each_with_index do |val, i|
   # binding.pry
     swapped = false
-      puts "i:#{i}, n:#{n}, - #{x.inspect}"
+      # puts "i:#{i}, n:#{n}, - #{x.inspect}"
     # (i..n).to_a.reverse.each do |j|
     ((i+1)..n).to_a.reverse.each do |j|
 
-      puts "i:#{i}, n:#{n}, j:#{j} - #{x.inspect}"
-      sleep 0.03
-      comparisons += 1
+      # puts "i:#{i}, n:#{n}, j:#{j} - #{x.inspect}"
+      # sleep 0.03
+      # comparisons += 1
       if x[j-1] < x[j-2]
         x[j-1], x[j-2] = x[j-2], x[j-1]
         swapped = true
@@ -22,12 +22,13 @@ def sortarray(x)
     break unless swapped
 
   end # each
-  puts comparisons
+  # puts comparisons
   x
 end # def
 
 
 array = (50..60).to_a.shuffle!
-puts '--------------'
-puts sortarray(array).inspect
+# puts '--------------'
+puts sortarray(array)
+
 
